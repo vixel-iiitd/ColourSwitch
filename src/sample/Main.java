4fcb31cad95921cfe104a4556e1f82b530ae4341
vixel-iiitd <55875752+vixel-iiitd@users.noreply.github.com>
2020-11-17 13:14:01 +0530

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
+public class Main extends Application {
+    public static MediaPlayer mediaPlayer;
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //addMusic();
+        Parent mainPage=FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+        Scene scene = new Scene(mainPage,1024,600);
+        primaryStage.setTitle("Color Switch");
+        primaryStage.setScene(scene);
+        //primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+
+}
